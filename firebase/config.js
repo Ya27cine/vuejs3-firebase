9bf5fd4270a0cf9c4ea55249b06d1085905f4b96
KHELIFA Yassine <prostam27@gmail.com>
2021-10-09 22:10:06 +0200

    Connection Vue 3 with Firebase

---
+++ firebase/config.js
@@ -0,0 +1,30 @@
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAWya4_EXBnfEzBrLJ1ehgpSZUEW54u68c",
+  
+    authDomain: "vuejs3-firebase-ef887.firebaseapp.com",
+  
+    projectId: "vuejs3-firebase-ef887",
+  
+    storageBucket: "vuejs3-firebase-ef887.appspot.com",
+  
+    messagingSenderId: "440356703624",
+  
+    appId: "1:440356703624:web:4e7fe35661750374927e3d"
+  
+  };
+
+  // init firebase :
+  firebase.initializeApp( firebaseConfig )
+
+  // init firestore
+  const projectFirestore = firebase.firestore();
+
+  export{
+      projectFirestore
+  }
